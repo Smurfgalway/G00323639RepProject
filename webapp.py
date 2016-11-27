f7f8b9e700339f3c9da3a66b0e4e925d868d0d99
smurfgalway <g00323639@gmit.ie>
2016-11-27 21:32:13 +0000

    flask set up in python file

---
+++ webapp.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+  
+@app.route('/name/<name>') 
+def fname(name): 
+	return 'Your name is ' + name
+ 
+@app.route("/")
+ 
+ 
+def hello():
+   return app.send_static_file('Index.html')
+
+if __name__ == "__main__":
+	app.run()
