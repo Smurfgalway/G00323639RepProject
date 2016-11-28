3fca974ae04352be964d293d20c7d5c29d64e1d1
smurfgalway <g00323639@gmit.ie>
2016-11-28 19:47:09 +0000

    static table for selection in database made & html page adjusted

--- Data/mydatabase.py
+++ Data/mydatabase.py
@@ -5,14 +5,36 @@ conn = sqlite3.connect('db.db')
 c = conn.cursor()
 
 def create_table():
-	c.execute('CREATE TABLE IF NOT EXISTS Ploting(unix REAL, datestamp TEXT, keyword TEXT, value Real)')
+	c.execute('CREATE TABLE IF NOT EXISTS Contents(number INT, name TEXT, price REAL)')
 	
 def data_entry():
-	c.execute("INSERT INTO 	Ploting VALUES(10, '28-11', 'hm', 07)")
-	conn.commit()
-	c.close()
-	conn.close()
+	c.execute("INSERT INTO 	Contents VALUES(1, 'ham', 2)")
+	c.execute("INSERT INTO 	Contents VALUES(2, 'cheese', 2)")
+	c.execute("INSERT INTO 	Contents VALUES(3, 'eggs', 1)")
+	c.execute("INSERT INTO 	Contents VALUES(4, 'milk', 2.5)")
+	c.execute("INSERT INTO 	Contents VALUES(5, 'bananas', 1)")
+	c.execute("INSERT INTO 	Contents VALUES(6, 'apples', 1)")
+	c.execute("INSERT INTO 	Contents VALUES(7, 'yogurt', 1)")
+	c.execute("INSERT INTO 	Contents VALUES(8, 'beer', 10)")
+	c.execute("INSERT INTO 	Contents VALUES(9, 'pizza', 5)")
+	c.execute("INSERT INTO 	Contents VALUES(10, 'wine', 10)")
+	c.execute("INSERT INTO 	Contents VALUES(11, 'bread', 4)")
+	c.execute("INSERT INTO 	Contents VALUES(12, 'sugar', 3)")
+	c.execute("INSERT INTO 	Contents VALUES(13, 'flour', 3)")
+	c.execute("INSERT INTO 	Contents VALUES(14, 'orange juice', 3)")
+	c.execute("INSERT INTO 	Contents VALUES(15, 'coke', 2.5)")
+	c.execute("INSERT INTO 	Contents VALUES(16, 'potatos', 4)")
+	c.execute("INSERT INTO 	Contents VALUES(17, 'carrots', 2)")
+	c.execute("INSERT INTO 	Contents VALUES(18, 'beans', 0.95)")
 	
+	conn.commit()	
+
+def create_table():
+	c.execute('CREATE TABLE IF NOT EXISTS ShoppingCart(number INT, name TEXT, price REAL)')
+
 create_table()
-data_entry()
+#data_entry()
+
+c.close
+conn.close
 	
--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import sqlite3
 app = Flask(__name__)
   
 @app.route('/name/<name>') 
